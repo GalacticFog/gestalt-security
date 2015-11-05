44a8f8f1462960a39efdb391aea93c9f47161c95
Christopher Baker <cgbaker@gmail.com>
2015-11-05 13:53:19 -0500

    remove table delete for account_store_types, will assume that database has been cleaned before migration

--- conf/db/migration/base/V1__create_reference_tables.sql
+++ conf/db/migration/base/V1__create_reference_tables.sql
@@ -2,7 +2,6 @@
 -- DIRECTORY_TYPE
 -- ----------------------------------------------------------------------------
 
-DROP TABLE IF EXISTS account_store_type CASCADE;
 CREATE TABLE account_store_type(
   id BIGSERIAL PRIMARY KEY,
   name TEXT NOT NULL,
