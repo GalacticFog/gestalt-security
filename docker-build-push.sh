c53efd560d571847ee5dd18040889d8f8938fef1
Christopher Baker <cgbaker@gmail.com>
2016-04-15 02:05:34 -0400

    update to build script

--- docker-build-push.sh
+++ docker-build-push.sh
@@ -1,5 +1,7 @@
 #!/bin/bash 
 
+export IMG=galacticfog.artifactoryonline.com/gestalt-security
+
 set -e
 # set -x
 
@@ -11,7 +13,7 @@ if [ $numChanges -gt 0 ]; then
   echo "You are dirty. You probably shouldn't be running this script."
   exit 1       
 fi
-if [ branch != "master" ]; then 
+if [ $branch != "master" ]; then 
   echo "Uh-oh. You appear not to be on the master branch. You probably shouldn't be running this script yet."
   exit 1
 fi
@@ -19,8 +21,6 @@ if [ `git rev-parse origin/master` != `git rev-parse HEAD` ]; then
   echo "Your master branch does not appear to agree with origin/master. You probably shouldn't be running this script yet."
   exit 1
 fi
-
-export IMG=galacticfog.artifactoryonline.com/gestalt-security
 
 export SHA=$(git rev-parse --short=8 HEAD)
 export VER=$(grep "^version" build.sbt | sed 's/.*:=[ ]*//' | sed 's/"//g')
@@ -34,7 +34,8 @@ cd target/docker/stage
 docker build -t $IMG:$TAG .
 echo "Pushing new image to artifactory..."
 docker push   $IMG:$TAG
-docker tag -f $IMG:$TAG $IMG:$VER
-docker push   $IMG:$VER
 
-echo -e "Built and pushed\n$IMG:$TAG\nand\n$IMG:$VER"
+echo Built and published: $IMG:$TAG
+echo If you want to update the version, run the following:
+echo    docker tag -f $IMG:$TAG $IMG:$VER
+echo    docker push   $IMG:$VER
